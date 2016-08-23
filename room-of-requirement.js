3aca557e3739d11261a5103fb64438cdd7dc73be
Adam Haile <aah@curveship.org>
2016-08-23 10:10:42 -0400

    preserving aborted overlay strategy for simpler approach

--- room-of-requirement.js
+++ room-of-requirement.js
@@ -7,20 +7,39 @@
     }
 })(function (require, exports) {
     "use strict";
-    var RoomOfRequirement = (...namespaces) => entrypoint(chain(namespaces)), entrypoint = (namespace) => (prod) => resolve(prod, namespace, {}), resolve = (prod, namespace, cache) => {
+    var RoomOfRequirement = (...namespaces) => entrypoint(hh.chain(namespaces)), entrypoint = (namespace) => (prod) => resolve(prod, namespace, hh.root()), resolve = (prod, namespace, cache) => {
         var deps = {}, result = prod(injector(namespace, cache, deps));
         return result;
     }, injector = (namespace, cache, deps) => new Proxy({}, { get: (_, name) => {
             var prod = namespace[name];
             return !prod ? missing(name) :
                 prod instanceof Function ?
-                    deps[name] = cache[name] = (name in cache ? cache[name] : resolve(prod, namespace, cache)) :
-                    injector(prod, cache[name] = cache[name] || {}, deps[name] = deps[name] || {});
-        } }), chain = (namespaces) => {
-        for (var i = 0; i < namespaces.length; i++)
-            namespaces[i]['__proto__'] = i ? namespaces[i - 1] : Object.create(null);
-        return namespaces[i - 1];
-    }, missing = (name) => { throw new Error("missing dependency: " + name); };
+                    (deps[name] = true, cache[name] = (name in cache ? cache[name] : resolve(prod, namespace, cache))) :
+                    injector(prod, hh.sub(cache, name), deps[name] = deps[name] || {});
+        } }), missing = (name) => { throw new Error("missing dependency: " + name); }, hh = {
+        chain: (namespaces) => {
+            for (var i = 0; i < namespaces.length; i++)
+                namespaces[i]['__proto__'] = i ? namespaces[i - 1] : hh.root();
+            return namespaces[i - 1];
+        },
+        root: () => Object.create(null),
+        depth: (h, name) => {
+            for (var i = 0; h && !Object.prototype.hasOwnProperty.call(h, name); i++, h = h.__proto__) { }
+            return h ? -1 : i;
+        },
+        set: (h, depth, key, value) => {
+            while (depth--)
+                h = h.__proto__;
+            h[key] = value;
+        },
+        sub: (h, name) => {
+            var top = h[name] = h[name] || hh.root(), sub = top;
+            while (h = h.__proto__) {
+                h[name] = sub = sub.__proto__ = h[name] || hh.root();
+            }
+            return top;
+        }
+    };
     Object.defineProperty(exports, "__esModule", { value: true });
     exports.default = RoomOfRequirement;
 });
--- room-of-requirement.ts
+++ room-of-requirement.ts
@@ -14,8 +14,8 @@ interface Entrypoint {
     <T>(prod : Production<T>) : T
 }
 
-var RoomOfRequirement = (...namespaces : Namespace[]) => entrypoint(chain(namespaces)),
-    entrypoint = (namespace : Namespace) : Entrypoint => <T>(prod : Production<T>) => resolve(prod, namespace, {} as Dependencies),
+var RoomOfRequirement = (...namespaces : Namespace[]) => entrypoint(hh.chain(namespaces)),
+    entrypoint = (namespace : Namespace) : Entrypoint => <T>(prod : Production<T>) => resolve(prod, namespace, hh.root()),
     resolve = (prod : Production<any>, namespace : Namespace, cache : Dependencies) => {
         var deps = {} as Dependencies,
             result = prod(injector(namespace, cache, deps));
@@ -26,14 +26,33 @@ var RoomOfRequirement = (...namespaces : Namespace[]) => entrypoint(chain(namesp
             var prod = namespace[name];
             return !prod ? missing(name) :
                 prod instanceof Function ? 
-                    deps[name] = cache[name] = (name in cache ? cache[name] : resolve(prod, namespace, cache)) :
-                injector(prod, cache[name] = cache[name] || {}, deps[name] = deps[name] || {});
+                    (deps[name] = true, cache[name] = (name in cache ? cache[name] : resolve(prod, namespace, cache))) :
+                injector(prod, hh.sub(cache, name), deps[name] = deps[name] || {});
         } }),
-    chain = (namespaces : Namespace[]) => {
-        for (var i = 0; i < namespaces.length; i++)
-            namespaces[i]['__proto__'] = i ? namespaces[i - 1] : Object.create(null);
-        return namespaces[i - 1];
-    },
-    missing : any = (name : string) => { throw new Error("missing dependency: " + name); };
+    missing : any = (name : string) => { throw new Error("missing dependency: " + name); },
+    hh = {
+        chain: (namespaces : Namespace[]) => {
+            for (var i = 0; i < namespaces.length; i++)
+                namespaces[i]['__proto__'] = i ? namespaces[i - 1] : hh.root();
+            return namespaces[i - 1];
+        },
+        root: () => Object.create(null),
+        depth: (h : any, name : string | number | symbol) => {
+            for (var i = 0; h && !Object.prototype.hasOwnProperty.call(h, name); i++, h = h.__proto__) {}
+            return h ? -1 : i;
+        },
+        set: (h: any, depth : number, key : string | number | symbol, value : any) => {
+            while (depth--) h = h.__proto__;
+            h[key] = value;
+        },
+        sub: (h : any, name : string | number | symbol) => {
+            var top = h[name] = h[name] || hh.root(),
+                sub = top;
+            while (h = h.__proto__) {
+                h[name] = sub = sub.__proto__ = h[name] || hh.root();
+            }
+            return top;
+        }
+    };
 
 export default RoomOfRequirement;
